c82306dd992c9f09ea8ad828e0dd611ce26941ce
maycon <maycon5591@gmail.com>
2022-02-24 23:52:34 -0300

    Layout alterado

--- src/pages/CreateCharPage/CreateCharPage.jsx
+++ src/pages/CreateCharPage/CreateCharPage.jsx
@@ -7,13 +7,8 @@ const CreateCharPage = () => {
         <div className='CreateCharPage'>
             <div className='CreateCharPage_panel'>
                 <h1 className='CreateCharPage_Title'>Game With Life</h1>
-                <h2 className='CreateCharPage_Title2'>Escolha seu avatar</h2>
-                <div className='CreateCharPage_avatarWrapper'></div>
-                <form className='CreateCharPage_Form' action="">
-                    <label htmlFor="name" >Nome</label>
-                    <input autocomplete="false"  type="text" id="name"/>
-                    <button type='submit'>Criar</button>
-                </form>
+                <h2 className='CreateCharPage_Title2'>Seja bem vindo, aventureiro!</h2>
+                <button className='CreateCharPage_Button'>Roll The Dice</button>
             </div>
         </div>
     )
