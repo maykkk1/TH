b4b9c86632ada595ac4ad730db81edf12c8964db
maycon <maycon5591@gmail.com>
2022-03-18 23:08:15 -0300

    validate create user OK

--- src/component/CreateCharModal/CharacteresContainer/CharacteresContainer.jsx
+++ src/component/CreateCharModal/CharacteresContainer/CharacteresContainer.jsx
@@ -5,18 +5,21 @@ import Character from "../Character/Character";
 function CharacteresContainer(props) {
   const maleClasses = [
     <Character
+      key={0}
       characterClass={"Warrior"}
       characterAvatar={"male_warrior.png"}
       setCharacterSelected={props.setCharacterSelected}
       characterSelected={props.characterSelected}
     />,
     <Character
+      key={1}
       characterClass={"Rogue"}
       characterAvatar={"male_rogue.png"}
       setCharacterSelected={props.setCharacterSelected}
       characterSelected={props.characterSelected}
     />,
     <Character
+      key={2}
       characterClass={"Mage"}
       characterAvatar={"male_mage.png"}
       setCharacterSelected={props.setCharacterSelected}
@@ -26,18 +29,21 @@ function CharacteresContainer(props) {
 
   const femaleClasses = [
     <Character
+      key={0}
       characterClass={"Warrior"}
       characterAvatar={"female_warrior.png"}
       setCharacterSelected={props.setCharacterSelected}
       characterSelected={props.characterSelected}
     />,
     <Character
+      key={1}
       characterClass={"Rogue"}
       characterAvatar={"female_rogue.png"}
       setCharacterSelected={props.setCharacterSelected}
       characterSelected={props.characterSelected}
     />,
     <Character
+      key={2}
       characterClass={"Mage"}
       characterAvatar={"female_mage.png"}
       setCharacterSelected={props.setCharacterSelected}
@@ -45,15 +51,13 @@ function CharacteresContainer(props) {
     />,
   ];
 
-  const [selectedGender, SetSelectedGender] = useState(femaleClasses)
+  const [selectedGender, SetSelectedGender] = useState(femaleClasses);
 
-  useEffect(()=>{
-    props.genderSelected === "female" 
-    ? SetSelectedGender(femaleClasses)
-    : SetSelectedGender(maleClasses)
-  },[props.genderSelected, props.characterSelected])
-
-
+  useEffect(() => {
+    props.genderSelected === "female"
+      ? SetSelectedGender(femaleClasses)
+      : SetSelectedGender(maleClasses);
+  }, [props.genderSelected, props.characterSelected]);
 
   return (
     <div className="CreateCharModal_CharacterOptionsSection">
--- src/component/Home/UserHUD/userHUD.jsx
+++ src/component/Home/UserHUD/userHUD.jsx
@@ -5,6 +5,7 @@ import GoldView from "../util/GoldView/GoldView";
 
 function UserHUD() {
   const { user } = useUser();
+  console.log(user)
   return (
     <div className="UserHUD">
       <div className="UserHUD_AvatarSection">
