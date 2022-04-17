2a8da1367670fb4329222aa5279d6cb396dda376
maycon <maycon5591@gmail.com>
2022-04-17 09:17:23 -0300

    rewards Style OK

--- src/component/Home/Rewards/RewardsModal/RewardsModal.jsx
+++ src/component/Home/Rewards/RewardsModal/RewardsModal.jsx
@@ -1,5 +1,7 @@
-import React, { PureComponent } from 'react';
+import React from 'react';
 import RewardsRadio from '../RewardsUtil/RewardsRadio';
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faCoins } from "@fortawesome/free-solid-svg-icons";
 import './style.css'
 
 function RewardsModal(props) {
@@ -10,13 +12,22 @@ function RewardsModal(props) {
             <div className='RewardsModal_Container'>
                 <span onClick={()=>{props.onClose()}} className="RewardsModal_Close">x</span>
                 <form action="">
-                    <h2>Rewards</h2>
+                    <h2>Nova recompensa</h2>
                     <form action="">
                         <label htmlFor="rewardName">Nome</label>
                         <input type="text" id='rewardName'/>
                         <RewardsRadio time={'15'}/>
                         <RewardsRadio time={'30'}/>
                         <RewardsRadio time={'60'}/>
+                        <span className='RewardsRadio'>
+                            <input id='radioNoTime' type="radio" name='timeRadio' />  
+                            <label htmlFor='radioNoTime'>Sem tempo</label>
+                        </span>
+                        <span className='RewardsCost'>
+                            <label htmlFor="rewardsCost"><FontAwesomeIcon icon={faCoins} /></label>
+                            <input type="number" value={0} id='rewardsCost' />
+                        </span>
+                        <button type='submit'>Criar</button>
                     </form>
                 </form>
             </div>
