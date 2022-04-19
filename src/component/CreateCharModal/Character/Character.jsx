3871eb46d3337e3cf0c2be138093f2aa062226b1
maycon <maycon5591@gmail.com>
2022-04-19 19:53:40 -0300

    class bonus OK

--- src/component/CreateCharModal/Character/Character.jsx
+++ src/component/CreateCharModal/Character/Character.jsx
@@ -1,7 +1,9 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./style.css";
 
 function Character(props) {
+  const [isBonusHoverVisible, SetIsBonusHoverVisible] = useState(false)
+  const [classBonus, SetClassBonus] = useState({xp:30, gold:10})
 
   const characterSelectedStyle = {
     boxShadow: "0 5px 10px #9CC0E7",
@@ -15,6 +17,17 @@ function Character(props) {
     }
   }
 
+  const bonusHover = () => {
+    isBonusHoverVisible 
+    ? SetIsBonusHoverVisible(false)
+    : SetIsBonusHoverVisible(true)
+  }
+
+  useEffect(()=>{
+    if(props.characterClass == 'Mage') SetClassBonus({xp:30, gold:10})
+    if(props.characterClass == 'Rogue') SetClassBonus({xp:10, gold:30})
+    if(props.characterClass == 'Warrior') SetClassBonus({xp:15, gold:15})
+  })
 
   return (
     <div className="Character">
@@ -26,12 +39,17 @@ function Character(props) {
         }}
         style={ChangeStyle()}
         className="Character_imgCharacterWrapper"
+        onMouseLeave={bonusHover}
+        onMouseEnter={bonusHover}
       >
         <img
           src={require(`../../../assests/images/characters/${props.characterAvatar}`)}
           alt=""
         />
       </div>
+      {isBonusHoverVisible
+      ?<span className="Character__bonus"><p>{`+ ${classBonus.xp}% xp`}</p><p>{`+ ${classBonus.gold}% gold`}</p></span>
+      : null}
     </div>
   );
 }
